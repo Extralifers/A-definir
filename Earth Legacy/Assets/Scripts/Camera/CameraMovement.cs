38f66f9665c6e8e09400d6732f281b920e797e95
valli22 <davidvacasmiguel@hotmail.com>
2016-07-03 17:17:27 +0200

    Copia del otro repositorio

---
+++ Earth Legacy/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    private GameObject player;
+    private Transform gameCamera;
+    private Transform user;
+    private float delayToFind = 0;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        user = player.transform;
+        gameCamera = this.transform;
+    }
+
+    void Update () {
+        
+        if (player == null)
+        {
+            LookForPlayer();
+            return;
+        }
+
+        //this.transform.position = player.transform.position;
+        gameCamera.position = new Vector3(user.position.x, user.position.y, -10);
+
+	}
+
+    void LookForPlayer()
+    {
+        if(delayToFind <= Time.time)
+        {
+            GameObject founded = GameObject.FindGameObjectWithTag("Player");
+            if(founded != null)
+            {
+                player = founded;
+            }
+            else
+            {
+                delayToFind = Time.time + 0.5f;
+            }
+        }
+    }
+
+}
